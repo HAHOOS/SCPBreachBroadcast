972f9acf40546330fe2d063f90a172b514acdf2b
gracj <gracj@ALIENWARE-15R4>
2023-08-12 21:59:28 +0200

    Add project files.

---
+++ SCPBreachBroadcast/EventHandlers.cs
@@ -0,0 +1,46 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using System.Threading.Tasks;
+
+namespace SCPBreachBroadcast
+{
+    internal class EventHandlers
+    {
+        [PluginEvent(ServerEventType.RoundStart)]
+        public async void RoundStarted()
+        {
+            await Task.Delay(1000);
+            string scps_string = "";
+            foreach (var player in Player.GetPlayers())
+            {
+                Log.Info($"{player.Nickname}: {player.Role.ToString()}");
+                if (player.Role.ToString().Contains("Scp"))
+                {
+                    if (scps_string == "")
+                    {
+                        scps_string += player.Role.ToString().ToUpper().Replace("P", "P ") + " ";
+                    }
+                    else
+                    {
+                        scps_string += " " + player.Role.ToString().ToUpper().Replace("P", "P ") + " ";
+                    }
+                }
+            }
+            if (scps_string != "")
+            {
+                Cassie.Message(scps_string + " have breached containment . All personel are advised to proceed with standard evacuation protocols", isSubtitles: true, isNoisy: true);
+            }
+            else if (scps_string == "")
+            {
+                Cassie.Message("The facility has been scanned . . 0 breaches detected . . Substantial threat to safety remains within the facility -- exercise caution", isSubtitles: true);
+            }
+        }
+
+        [PluginEvent(ServerEventType.PlayerChangeRole)]
+        public void PlayerChangeRole(Player player)
+        {
+            Log.Info(player.Role.ToString());
+        }
+    }
+}
---
+++ SCPBreachBroadcast/SCPBreachBroadcast.cs
@@ -0,0 +1,21 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Events;
+using System;
+
+namespace SCPBreachBroadcast
+{
+    public class SCPBreachBroadcast
+    {
+        public static SCPBreachBroadcast Instance { get; private set; }
+
+        [PluginEntryPoint("SCPBreachBroadcast", "1.0.0", "A plugin that automatically broadcasts a message about breach", "HAHOOS")]
+        void LoadPlugin()
+        {
+            Instance = this;
+            EventManager.RegisterEvents<EventHandlers>(this);
+
+            var handler = PluginHandler.Get(this);
+        }
+    }
+}
